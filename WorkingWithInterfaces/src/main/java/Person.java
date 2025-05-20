2bb966a33fccd74fda8a412e56141c20a18dbb06
Izel Ozhan <izelozhan@gmail.com>
2025-05-20 09:43:36 -0500

    workbook-6 exercise-2

--- WorkingWithInterfaces/src/main/java/Person.java
+++ WorkingWithInterfaces/src/main/java/Person.java
@@ -1,2 +1,20 @@
-public class Person {
+public class Person implements Comparable<Person> {
+    String firstName;
+    String lastName;
+    int age;
+
+    public Person(String lastName, String firstName, int age) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    public int compareTo(Person p) {
+        int last = this.lastName.compareTo(p.lastName);
+        int first =  this.firstName.compareTo(p.firstName);
+
+        return last == 0 ? first : last;
+    }
+
+
 }
