d5efdf4f09a13f517381a574b04989f57470d515
Izel Ozhan <izelozhan@gmail.com>
2025-05-22 13:27:14 -0500

    mod-4 exercise-2

--- Streams2/src/main/java/com/pluralsight/streams/Program.java
+++ Streams2/src/main/java/com/pluralsight/streams/Program.java
@@ -3,12 +3,12 @@ package com.pluralsight.streams;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Person> crowd = new ArrayList<>();
-        List<Person> matched = new ArrayList<>();
 
         crowd.add(new Person("Emma", "Johnson", 25));
         crowd.add(new Person("Liam", "Smith", 24));
@@ -21,45 +21,29 @@ public class Program {
         crowd.add(new Person("Elijah", "Wilson", 60));
         crowd.add(new Person("Isabella", "Moore", 70));
 
-        System.out.println("Enter a name to search you can search with firstname or lastname: ");
+        System.out.println("Enter a name to search you can search with first-name or last-name: ");
         String searchInput = scanner.nextLine().trim().toLowerCase();
 
+        //steam version
+        List<Person> match = crowd.stream().filter(p -> p.getFirstName().trim().equalsIgnoreCase(searchInput) || p.getLastName().trim().equalsIgnoreCase(searchInput)).toList();
 
-        for (Person p : crowd) {
-            String firstName = p.getFirstName().trim().toLowerCase();
-            String lastName = p.getLastName().trim().toLowerCase();
-
-            if (firstName.equals(searchInput) || lastName.equals(searchInput)) {
-                matched.add(p);
-                System.out.println(p + "is added to your matched list!");
-            }
-        }
-
-        if (matched.isEmpty()) {
+        //print message
+        if (match.isEmpty()) {
             System.out.println("No matches found!");
         } else {
             System.out.println("Your matched list: ");
-            for (Person p : matched) {
+            for (Person p : match) {
                 System.out.println(p.firstName + " " + p.lastName);
             }
         }
+
         //calculate average orj list
-        int totalAge = 0;
-        int oldest = 0;
-        int youngest = Integer.MAX_VALUE; // if we
-
-        for (Person p : crowd){
-            totalAge += p.getAge();
-
-            if (p.getAge() > oldest){
-                oldest = p.age;
-            }
-            if (p.getAge() < youngest){
-                youngest = p.age;
-            }
-
-        }
+        int totalAge = crowd.stream().map(Person::getAge).reduce(0, (a, b) -> a + b);
         int average = totalAge / crowd.size();
+        int oldest = crowd.stream().map(Person::getAge).reduce(0, Integer::max);
+        int youngest = crowd.stream().map(Person::getAge).reduce(Integer.MAX_VALUE, Integer::min);
+
+
         System.out.println("Average age is : " + average);
 
         System.out.println("Oldest age is : " + oldest);
